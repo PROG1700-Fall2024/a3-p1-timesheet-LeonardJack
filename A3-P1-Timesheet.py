d3a1d9f1ec15df07114590e2615b16a164d895f2
LeonardJack <w0499622@nscc.ca>
2024-11-21 10:34:47 -0400

    Code With comments

--- A3-P1-Timesheet.py
+++ A3-P1-Timesheet.py
@@ -8,26 +8,26 @@
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
-    hours = []
-    totalHours = 0
+    hours = [] #empty list, used for keeping track of our hours 
+    totalHours = 0 #our total hours starts at zero
     for i in range(5):
-        dayHour = int(input("Enter hours worked on day {0} ".format(i+1)))
-        totalHours += dayHour
-        hours.append(dayHour)
+        dayHour = int(input("Enter hours worked on day {0} ".format(i+1))) #collecting inputs by iterating through each work day (monday to friday)
+        totalHours += dayHour #adds the hours to our total hours 
+        hours.append(dayHour) #appends our hours value to our list
 
     print("----------------------------------------\nYour most hours worked was on:")
-    mostHours = max(hours)
+    mostHours = max(hours) #gets the highest number in the hours list 
     for i in range(5):
-        if hours[i] == mostHours :
-            print("Day {0} where you worked for {1} hours".format(i+1, hours[i]))
+        if hours[i] == mostHours : # you will only go to the print statement if the given day you worked the most hours 
+            print("Day {0} where you worked for {1} hours".format(i+1, hours[i])) #prints the day(s) you worked the most hours 
     
-    print("----------------------------------------\nYour total number of hours worked is {0}".format(totalHours))
-    print("Your average hours worked was {}\n----------------------------------------".format(totalHours/5))
-    print("Days you slacked off (worked less than 7 hours)")
+    print("----------------------------------------\nYour total number of hours worked is {0}".format(totalHours)) #prints total hours worked
+    print("Your average hours worked was {}\n----------------------------------------".format(totalHours/5))# prints your average hours worked (is calculated in the format statement)
+    print("Days you slacked off (worked less than 7 hours)") #this print statement precedes the next bit 
 
-    for i in range(5):
-        if hours[i] < 7:
-            print("Day {0} where you worked {1} hours".format(i+1, hours[i]))
+    for i in range(5):  #used to iterate through each day and print the days you slacked off
+        if hours[i] < 7:    #you slacked off if you worked less than 7 hours
+            print("Day {0} where you worked {1} hours".format(i+1, hours[i])) #formatted print statement
 
 
 
